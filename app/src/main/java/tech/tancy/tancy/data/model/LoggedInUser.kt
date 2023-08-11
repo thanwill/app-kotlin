6fba3a1aeabd1a0e4184e67d9ac84f58993268a3
Jonathan Pereira <jonathan14willian@icloud.com>
2023-08-11 16:46:38 -0300

    Implementação do Firebase, Autentication, CadastroActivity

---
+++ app/src/main/java/tech/tancy/tancy/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package tech.tancy.tancy.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
---
+++ app/src/main/java/tech/tancy/tancy/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package tech.tancy.tancy.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
